e66f00f5c2f01f45fc880ed5ea6eb164f7a40194
paulamarie00 <ayaba.rodriguez@aig.com>
2025-01-30 19:31:35 +0000

    begin

--- Main.py
+++ Main.py
@@ -21,5 +21,33 @@ int() - cast to integer
 float() - cast to float
 """
 
+# Formatted Strings 
+n1 = 5
+n2 = 3
+res = n1 + n2
+out = f"{n1}+{n2}= {n1+n2}"
+print(out) 
+
+
+# Logical operators
+# Code Block
+
+n1=5
+n2=5
+if n1<n2:
+    print("Lesser")
+    print("<"*6)
+elif n1==n2:
+    print("Equal")
+    print('=====')
+else:
+    print("Greater")
+    print(">"*len("Greater"))
+
+# blocks in Python use indentation
+# used in if statements/ function def
+
+f = 17.4
+print(type(f))
 
 
