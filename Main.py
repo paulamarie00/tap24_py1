7daf3c890ee34733577dddc36041614e5c72a9cc
paulamarie00 <ayaba.rodriguez@aig.com>
2025-01-30 19:07:01 +0000

    begin

---
+++ Main.py
@@ -0,0 +1,25 @@
+# Python 1
+# Basic Data Types
+
+h = "hello"
+w = "world"
+
+print(h,w)
+print(h+w)
+
+n1 = 5
+# print(h+n1)
+
+# How to concatenate str with number?
+# Typecasting 
+
+print(h+str(n1))
+
+"""
+str() - cast to string 
+int() - cast to integer
+float() - cast to float
+"""
+
+
+
